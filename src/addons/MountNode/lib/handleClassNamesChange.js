14def819eba96f42ff566ad47018b0ae1c05e57f
Alex Lau <laux0083@gmail.com>
2018-08-29 23:21:29 -0500

    Add a setToArray method that works with IE

--- src/addons/MountNode/lib/handleClassNamesChange.js
+++ src/addons/MountNode/lib/handleClassNamesChange.js
@@ -5,7 +5,18 @@ import computeClassNamesDifference from './computeClassNamesDifference'
 
 const prevClassNames = new Map()
 
+function setToArray(set) {
+  var index = -1,
+      result = (set && set.size) ? Array(set.size) : [];
+
+  (set || []).forEach(function(value) {
+    result[++index] = value;
+  });
+  return result;
+}
+
 const handleClassNamesChange = (node, components) => {
+  components = setToArray(components);
   const currentClassNames = computeClassNames(components)
   const [forAdd, forRemoval] = computeClassNamesDifference(
     prevClassNames.get(node),
